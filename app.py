1ce062f3fc0f7212fe86b8331d760ac6bf21f6ae
AnakhaSNair <anakhasanthosh204@gmail.com>
2026-03-01 15:52:06 +0530

    "Ready for deployment"

--- app.py
+++ app.py
@@ -320,5 +320,6 @@ def download_pdf():
     return send_file("story.pdf", as_attachment=True)
 
 # ---------------- MAIN ---------------- #
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
